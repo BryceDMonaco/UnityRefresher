31f979262e01dec0fe3f90e0342da9c653370991
Bryce Monaco <brycedmonaco@gmail.com>
2022-01-20 19:21:43 -0800

    Completed challenge 2

--- Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -6,13 +6,24 @@ public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
 
+    private bool waitingToSpawnAgain = false;
+
+    private int spawnDelay = 1;
     // Update is called once per frame
     void Update()
     {
         // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && !waitingToSpawnAgain)
         {
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+
+            waitingToSpawnAgain = true;
+            Invoke("EnableSpawn", spawnDelay);
         }
+    }
+
+    void EnableSpawn()
+    {
+        waitingToSpawnAgain = false;
     }
 }
--- Prototype 2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Prototype 2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -16,7 +16,7 @@ public class SpawnManagerX : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
+        Invoke("SpawnRandomBall", startDelay);
     }
 
     // Spawn random ball at random x position at top of play area
@@ -25,8 +25,12 @@ public class SpawnManagerX : MonoBehaviour
         // Generate random ball index and random spawn position
         Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
 
+        int ballPrefabIndex = Random.Range(0, ballPrefabs.Length);
+        
         // instantiate ball at random spawn location
-        Instantiate(ballPrefabs[0], spawnPos, ballPrefabs[0].transform.rotation);
+        Instantiate(ballPrefabs[ballPrefabIndex], spawnPos, ballPrefabs[ballPrefabIndex].transform.rotation);
+        
+        Invoke("SpawnRandomBall", Random.Range(1, 5));
     }
-
+    
 }
